43e7cb97803c0ac1255cd9075a345b597add8eca
Damian de Vega <damiandevega@gmail.com>
2020-09-17 13:38:03 -0400

    refactoring webpack configs to use a base config file and not repeat code

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,9 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const baseConfig = require('./webpack.base');
+
+const config = {
   // Inform webpack that we are building a bundle for nodeJS, rather than for the browser
   target: 'node',
 
@@ -12,22 +15,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            ['env', { targets: { browsers: ['last 2 versions'] } }],
-          ],
-        },
-      },
-    ],
-  },
 };
+
+module.exports = merge(baseConfig, config);
