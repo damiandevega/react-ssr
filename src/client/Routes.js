65e2c57fc3fe597a1cae307c57a261ab54dd331d
Damian de Vega <damiandevega@gmail.com>
2020-09-21 17:30:18 -0400

    import cleanup

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import HomePage from './pages/HomePage';
-import UsersListPage, { loadData } from './pages/UsersListPage';
+import UsersListPage from './pages/UsersListPage';
 
 export default [
   {
