a5ca52542dfbd3eb6b5a005d635149bc2e56bb61
Damian de Vega <damiandevega@gmail.com>
2020-09-24 15:47:38 -0400

    adding navigation links in Header component

--- public/bundle.js
+++ public/bundle.js
@@ -39898,6 +39898,17 @@ var Header = function Header(_ref) {
   var auth = _ref.auth;
 
   console.log('My authorization status is ', auth);
+
+  var authButton = auth ? _react2.default.createElement(
+    'a',
+    { href: '/api/logout' },
+    'Logout'
+  ) : _react2.default.createElement(
+    'a',
+    { href: '/api/auth/google' },
+    'Login'
+  );
+
   return _react2.default.createElement(
     'div',
     null,
@@ -39905,6 +39916,21 @@ var Header = function Header(_ref) {
       _reactRouterDom.Link,
       { to: '/' },
       'React SSR'
+    ),
+    _react2.default.createElement(
+      'div',
+      null,
+      _react2.default.createElement(
+        _reactRouterDom.Link,
+        { to: '/users' },
+        'Users'
+      ),
+      _react2.default.createElement(
+        _reactRouterDom.Link,
+        { to: '/admins' },
+        'Admins'
+      ),
+      authButton
     )
   );
 };
